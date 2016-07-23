3b771ae22e89c71c3e87063162641e81b5b7745a
wkylin <wkylin@sina.cn>
2016-07-23 21:54:46 +0800

    gitignore

--- js/index/controllers.js
+++ js/index/controllers.js
@@ -2,6 +2,14 @@
 
     angular.module('index.controllers', [])
         .controller("matchCtrl", function ($rootScope, $scope) {
+            $scope.num = 1;
+            var list = [
+                {"name": "白银", "percent": 0.7333, "amount": 3000},
+                {"name": "黄金", "percent": 0.1333, "amount": 3000},
+                {"name": "青铜", "percent": 0.1333, "amount": 3000}
+                
+            ];
+            $scope.propertyMatch = list;
             $scope.initFn = function () {
                 for (var i = 1; i < list.length; i++) {
                     if (list[i - 1].startPosition == 360) {
@@ -23,22 +31,11 @@
                     }
                 }
             }
-
-            $scope.num = 1;
-            //总资产
-            $scope.totalAssets = 0;
-            $scope.totalAssets = 100000;
-            var list = [
-                {"name": "新手专享", "percent": 0.25, "amount": 3000},
-                {"name": "泰金宝1", "percent": 0.25, "amount": 7000},
-                {"name": "泰金宝2", "percent": 0.25, "amount": 8000},
-                {"name": "泰金宝3", "percent": 0.25, "amount": 9000}
-            ];
-            $scope.propertyMatch = list;
+            
             if (list.length != 0) {
                 for (var i in list) {
                     if (i == 0) {
-                        list[i].startPosition = 0;//计算起始位置
+                        list[i].startPosition = 360;//计算起始位置
                     } else {
                         list[i].startPosition = list[i - 1].startPosition + list[i - 1].percent * 360;//计算起始位置
                         if (list[i].startPosition >= 360) {
@@ -47,38 +44,32 @@
                     }
                     list[i].rotateDeg = list[i].percent * 360;//计算旋转度数
                     if (list[i].percent > 0.5) {
-                        list[i].className = "gt50";
+                        list[i].className = "more-half";
                     } else {
                         list[i].className = "";
                     }
-                    list[i].class_bg = "chart_bg_" + i;
-                    list[i].property_font = "property_font_" + i;
-
+                    list[i].chartItem = "chart-item-" + i;
+                    list[i].activeItem = "active-item-" + i;
                 }
                 $scope.initFn();
-                $scope.propertyMatch = list;
-                $scope.isShow = true;
-            } else {
-                $scope.isShow = false;
-                $scope.propertyMatch = list
             }
             //滑动
             var angle = 0;
             var totalAngle = 0;
-            touch.on('#chart-loading', 'touchstart', function (ev) {
+            touch.on('#J_chartBox', 'touchstart', function (ev) {
                 ev.startRotate();
                 ev.originEvent.preventDefault();
             });
-            touch.on('#chart-loading', 'rotate', function (ev) {
+            touch.on('#J_chartBox', 'rotate', function (ev) {
                 console.log("rotate");
                 totalAngle = angle + ev.rotation;
                 if (ev.fingerStatus === 'end') {
                     angle = angle + ev.rotation;
                 }
-                document.getElementById("target").style.webkitTransform = 'rotate(' + totalAngle + 'deg)';
+                document.getElementById("J_chartBox").style.webkitTransform = 'rotate(' + totalAngle + 'deg)';
             });
 
-            touch.on('#chart-loading', 'touchend', function (ev) {
+            touch.on('#J_chartBox', 'touchend', function (ev) {
                 console.log("旋转角度：" + totalAngle);
                 var n = totalAngle % 360;
                 console.log(n);
@@ -114,7 +105,7 @@
                         console.log("顺时针3第几个：" + $scope.num);
                         $scope.$apply();
                     }
-                } else {              //逆时针旋转
+                } else {//逆时针旋转
                     if (n < 0 && n > -180) {
                         n = -n + 180;
                         for (var i = 1; i < list.length; i++) {
